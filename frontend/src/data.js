e57556f8786a76c1716a883b630b747da30c5a2f
Hans Bernadin <has.bernadin@outlook.com>
2020-10-09 23:09:53 -0400

    Share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [
+        {
+           _id: '1',
+           name: 'Hanes Mens Long-Sleeve ComfortSoft T-Shirt', 
+           category: 'Shirts',
+           image: '/images/p1.jpg',
+           price: 100,
+           brand: 'Nike',
+           rating: 4.5,
+           numReviews: 10,
+           description: 'high quality products'
+        },
+
+        {
+            _id: '2',
+            name: 'Hanes Mens Long-Sleeve ComfortSoft T-Shirt', 
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 200,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'high quality products'
+         },
+
+         {
+            _id: '3',
+            name: 'Hanes Mens Long-Sleeve ComfortSoft T-Shirt', 
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 300,
+            brand: 'Nike',
+            rating: 4.4,
+            numReviews: 10,
+            description: 'high quality products'
+         },
+
+         {
+            _id: '4',
+            name: 'Hanes Mens Long-Sleeve ComfortSoft T-Shirt', 
+            category: 'Shirts',
+            image: '/images/p4.jpg',
+            price: 400,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality products'
+         },
+
+         {
+            _id: '5',
+            name: 'Hanes Mens Long-Sleeve ComfortSoft T-Shirt', 
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 500,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 15,
+            description: 'high quality products'
+         },
+
+         {
+            _id: '6',
+            name: 'Hanes Mens Long-Sleeve ComfortSoft T-Shirt', 
+            category: 'Shirts',
+            image: '/images/p6.jpg',
+            price: 600,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 16,
+            description: 'high quality products'
+         },
+    ],
+};
+
+export default data;
